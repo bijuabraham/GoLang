e3634377893037b696c8e68bf608afa958aac7cc
Biju Abraham <github@bijuabraham.com>
2021-01-19 22:07:15 -0800

    more test

--- Cards/deck_test.go
+++ Cards/deck_test.go
@@ -9,4 +9,10 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 52 {
 		t.Errorf("Expected Deck length of 52 but got %v", len(d))
 	}
+	if d[0] != "Spades of Ace" {
+		t.Errorf("First item in Deck should be Spades of Ace but got %v", d[0])
+	}
+	if d[len(d)-1] != "Hearts of Two" {
+		t.Errorf("Last item in Deck should be Hearts of Two but got %v", d[len(d)-1])
+	}
 }
