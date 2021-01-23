89cc09de962496cc5a147fb7cedd66efc9a55c27
Biju Abraham <github@bijuabraham.com>
2021-01-23 10:15:12 -0800

    assignment 2 - Interfaces

---
+++ Shapes Assignment/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Areas interface {
+	area() float64
+}
+
+type triangle struct {
+	height float64
+	base   float64
+}
+type square struct {
+	sideLength float64
+}
+
+func main() {
+	t := triangle{height: 2.5, base: 3.5}
+	printArea(t)
+
+	s := square{sideLength: 2.5}
+	printArea(s)
+}
+
+func (s square) area() float64 {
+	return s.sideLength * s.sideLength
+}
+
+func (r triangle) area() float64 {
+	return r.base * r.height
+}
+
+func printArea(a Areas) {
+	fmt.Println(a)
+	fmt.Printf("area     : %f \n", a.area())
+}
